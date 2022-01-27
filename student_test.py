71e5884bc04f2b30859306e21cf6c107dc3310c6
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-01-27 13:43:09 -0500

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -4,7 +4,7 @@ import student
 
 
 def test_default():
-    input_values=['5.4']
+    input_values=['3.14159268','3']
     output=[]
 
     def mock_input(s=None):
@@ -20,10 +20,10 @@ def test_default():
 
     student.main()
 
-    assert '5.4' in output[1] and '            5.4' in output[1] and '5.4         ' in output[1] and '005.4' in output[1]
+    assert '3.142' == output[2],"Output the number only"
     
- def test_verify():
-    input_values=['16.3']
+def test_more_decimals():
+    input_values=['3.14159268','4']
     output=[]
 
     def mock_input(s=None):
@@ -39,6 +39,99 @@ def test_default():
 
     student.main()
 
-    assert '16.3' in output[1] and '           16.3' in output[1] and '16.3        ' in output[1] and '016.3' in output[1]
- 
+    assert '3.1416' == output[2], "Output the number only"
+    
+ def test_tau():
+    input_values=['6.283185','5']
+    output=[]
 
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert '6.28319' == output[2], "Output the number only"
+
+ def test_tau_two():
+    input_values=['6.283185','2']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert '6.28' == output[2], "Output the number only"
+    
+ def test_tau_whole():
+    input_values=['6.283185','0']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert '6' == output[2] or '6.0' == output[2], "Output the number only"
+    
+ def test_nines():
+    input_values=['9.9999','2']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert '10' == output[2] or '10.0' == output[2], "Output the number only"
+    
+ def test_nine_off():
+    input_values=['9.991','2']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert '9.99' == output[2] or '9.99' == output[2], "Output the number only"
